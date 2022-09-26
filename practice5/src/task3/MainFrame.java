1daa630a1df7a88287ad2be2cb33426e6dce52ab
kacchasu <7.germania.7@gmail.com>
2022-09-26 20:23:25 +0300

    dd

--- practice5/src/task3/MainFrame.java
+++ practice5/src/task3/MainFrame.java
@@ -11,16 +11,11 @@ public class MainFrame extends JFrame {
 
     private JLabel imgLabel;
 
-    private MainFrame(){
+    public MainFrame(String arg){
         this.setSize(1200, 1200);
-        imgLabel = new JLabel("", new ImageIcon(getClass().getResource("1.jpg")), SwingConstants.CENTER);
+        imgLabel = new JLabel("", new ImageIcon(getClass().getResource(arg)), SwingConstants.CENTER);
         getContentPane().add(imgLabel);
     }
 
-    public static void main(String[] args) {
-        MainFrame mf = new MainFrame();
-        mf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        mf.setVisible(true);
 
-    }
 }
